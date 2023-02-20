abe28f772ae925bb3bda0d196bedd5e43fc3ae5d
SC-V <37342773+SC-V@users.noreply.github.com>
2023-02-20 15:20:05 -0600

    Update get_warehouse_report.py

--- get_warehouse_report.py
+++ get_warehouse_report.py
@@ -215,7 +215,7 @@ option = st.sidebar.selectbox(
 def get_cached_report(period):
 
     if option == "Monthly":
-        report = get_report(period, start_="2022-12-31", end_="2023-01-31")
+        report = get_report(period, start_="2023-01-31", end_="2023-02-28")
     else:
         report = get_report(period)
     df_rnt = report[report['status'] != 'cancelled']
