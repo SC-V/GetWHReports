588b5c79745e8814c80fb44d162e343300f907df
SC-V <37342773+SC-V@users.noreply.github.com>
2023-06-16 17:02:03 -0600

    Update get_warehouse_report.py

--- get_warehouse_report.py
+++ get_warehouse_report.py
@@ -112,8 +112,8 @@ def get_report(option="Today", start_=None, end_=None) -> pandas.DataFrame:
     client_timezone = "America/Chihuahua"
 
     if option == "Monthly":
-        start_ = "2023-05-01"
-        end_ = "2023-05-31"
+        start_ = "2023-06-01"
+        end_ = "2023-06-31"
         today = datetime.datetime.now(timezone(client_timezone))
         date_from_offset = datetime.datetime.fromisoformat(start_).astimezone(
             timezone(client_timezone)) - datetime.timedelta(days=2)
